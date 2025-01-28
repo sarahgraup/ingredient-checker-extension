ad4e35bc22819ebf9387716e1d9c402e6e34c0ca
sarahgraup <sarahgraup@g.ucla.edu>
2025-01-28 14:48:24 -0800

    added function for ingredient chcker

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,60 +1,105 @@
-import React from 'react';
-import { Grid, Box, Button } from '@mui/material';
+import React, {useState} from 'react';
+import { Grid, Box, Button, Divider, Typography } from '@mui/material';
 import { findIngredients } from 'utils/ingredients';
-
+import SpinnerButton from 'components/SpinnerButton';
+import Results from 'components/Results';
 /**
  * click check ingreidnet send message to content script
  * 
  * 
  */
+
+
 export default function Popup() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [ingredients, setIngredients] = useState<string[]>([]);
   
   const analyze = () => {
+    setIsLoading(true);
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTab = tabs[0];
       if (!currentTab?.id) {
-        console.error("No active tab found");
+        console.error('No active tab found');
         return;
       }
-      console.log("Sending message to tab:", currentTab.id);
+      console.log('Sending message to tab:', currentTab.id);
       //dynamically injects content script
       chrome.scripting.executeScript({
         target: { tabId: currentTab.id },
-        files:["js/contentScript.js"],
+        files:['js/contentScript.js'],
       },
-        () => {
-           if (chrome.runtime.lastError) {
-             console.error("Runtime error:", chrome.runtime.lastError);
-             return;
-           }
-          console.log("content script injected. sending message");
-          chrome.tabs.sendMessage(
+      () => {
+        if (chrome.runtime.lastError) {
+          setIsLoading(false);
+          console.error('Runtime error:', chrome.runtime.lastError);
+          return;
+        }
+        console.log('content script injected. sending message');
+        chrome.tabs.sendMessage(
             currentTab.id!,
-            { type: "scrapeHtml" },
+            { type: 'scrapeHtml' },
             (response) => {
               if (chrome.runtime.lastError) {
-                console.error("Runtime error:", chrome.runtime.lastError);
+                setIsLoading(false);
+                console.error('Runtime error:', chrome.runtime.lastError);
                 return;
               }
-              console.log("Received response:", response);
-              findIngredients(response.ingredients);
+              console.log('Received response:', response);
+              if (response.success) {
+                const foundIngredients = findIngredients(response.ingredients);
+                setIngredients(foundIngredients);
+                setIsLoading(false);
+                
+              } else {
+                setIsLoading(false);
+                setIngredients(['no ingredients found']);
+              }
+              
             }
-          );
+        );
 
         
-      })
+      });
       
     });
   };
 
-    return (
-
-      <Grid container sx={{ width: '100px', height: '100px' }}>
-        <Box>
-          <Button onClick={analyze}>Check Ingredients</Button>
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        display: 'flex',
+        width: '300px',
+        height: '200px',
+        borderRadius: '50%',
+      }}
+    >
+      <Grid item xs={12}>
+        <Box sx={{ padding: '4%' }}>
+          <Typography variant="h1">Ingredient Checker</Typography>
         </Box>
+        <Divider />
       </Grid>
-    )
+
+      <Grid item xs={12}>
+        {/* <SpinnerButton
+          onClick={analyze}
+          color="primary"
+          variant="contained"
+          isLoading={isLoading}
+        >
+          Check Ingredients
+        </SpinnerButton> */}
+        <Button color="primary" variant="contained" onClick={analyze}>
+          Check Ingredients
+        </Button>
+        {ingredients && (
+          <Results ingredients={ingredients}/>
+        )}
+      </Grid>
+    </Grid>
+  );
 
     
 };
--- src/utils/ingredients.ts
+++ src/utils/ingredients.ts
@@ -1,171 +1,171 @@
 export const comedogenicIngredients = new Set([
-  "dioctyl succinate",
-  "disodium monooleamido peg 2-sulfosuccinate",
-  "glyceryl stearate se",
-  "glyceryl-3 diisostearate",
-  "hexadecyl alcohol",
-  "hydrogenated vegetable oil",
-  "isodecyl oleate",
-  "isosteraric acid",
-  "lauroyl lysine",
-  "linseed oil",
-  "myristyl lactate",
-  "octyl palmitate",
-  "octyl stearate",
-  "oleth-3",
-  "peg 200 dilaurate",
-  "ppg 2 myristyl propionate",
-  "polyglyceryl-3 diisostearate",
-  "sea whip extract",
-  "shark liver oil",
-  "solulan 16",
-  "sorbitan oleate",
-  "steareth 10",
-  "stearyl heptanoate",
-  "sulfated castor oil",
-  "wheat germ glyceride",
-  "acetylated lanolin",
-  "algae extract",
-  "algin",
-  "butyl stearate",
-  "carrageenan",
-  "cetyl acetate",
-  "cetearyl alcohol",
-  "ceteareth 20",
-  "coal tar",
-  "cocoa butter",
-  "coconut oil",
-  "colloidal sulfur",
-  "cotton seed oil",
-  "decyl oleate",
-  "ethylhexyl palmitate",
-  "isocetyl alcohol",
-  "isocetyl stearate",
-  "isopropyl isostearate",
-  "isopropyl linolate",
-  "isopropyl myristate",
-  "isopropyl palmitate",
-  "isostearyl isostearate",
-  "isostearyl neopentanoate",
-  "kelp",
-  "lanolin",
-  "lauric acid",
-  "mink oil",
-  "myristic acid",
-  "myristyl myristate",
-  "oleic acid",
-  "oleyl alcohol",
-  "palm oil",
-  "potassium chloride",
-  "propylene glycol monostearate",
-  "red dyes",
-  "shea butter",
-  "sodium lauryl sulfate",
-  "sodium laureth sulfate",
-  "soybean oil",
-  "stearic acid",
-  "wheat germ oil",
-  "xylene"
+  'dioctyl succinate',
+  'disodium monooleamido peg 2-sulfosuccinate',
+  'glyceryl stearate se',
+  'glyceryl-3 diisostearate',
+  'hexadecyl alcohol',
+  'hydrogenated vegetable oil',
+  'isodecyl oleate',
+  'isosteraric acid',
+  'lauroyl lysine',
+  'linseed oil',
+  'myristyl lactate',
+  'octyl palmitate',
+  'octyl stearate',
+  'oleth-3',
+  'peg 200 dilaurate',
+  'ppg 2 myristyl propionate',
+  'polyglyceryl-3 diisostearate',
+  'sea whip extract',
+  'shark liver oil',
+  'solulan 16',
+  'sorbitan oleate',
+  'steareth 10',
+  'stearyl heptanoate',
+  'sulfated castor oil',
+  'wheat germ glyceride',
+  'acetylated lanolin',
+  'algae extract',
+  'algin',
+  'butyl stearate',
+  'carrageenan',
+  'cetyl acetate',
+  'cetearyl alcohol',
+  'ceteareth 20',
+  'coal tar',
+  'cocoa butter',
+  'coconut oil',
+  'colloidal sulfur',
+  'cotton seed oil',
+  'decyl oleate',
+  'ethylhexyl palmitate',
+  'isocetyl alcohol',
+  'isocetyl stearate',
+  'isopropyl isostearate',
+  'isopropyl linolate',
+  'isopropyl myristate',
+  'isopropyl palmitate',
+  'isostearyl isostearate',
+  'isostearyl neopentanoate',
+  'kelp',
+  'lanolin',
+  'lauric acid',
+  'mink oil',
+  'myristic acid',
+  'myristyl myristate',
+  'oleic acid',
+  'oleyl alcohol',
+  'palm oil',
+  'potassium chloride',
+  'propylene glycol monostearate',
+  'red dyes',
+  'shea butter',
+  'sodium lauryl sulfate',
+  'sodium laureth sulfate',
+  'soybean oil',
+  'stearic acid',
+  'wheat germ oil',
+  'xylene'
 ]);
 
 // Mappings for alternative names/INCI names of the SAME ingredients
 export const nameMappings: { [key: string]: string } = {
   // Alternative names for acetylated lanolin
-  "acetylated lanolin alcohol": "acetylated lanolin",
+  'acetylated lanolin alcohol': 'acetylated lanolin',
   
   // Alternative names for algae and seaweed derivatives
-  "chlorella": "algae extract",
-  "plankton": "algae extract",
-  "seaweed": "algae extract",
-  "spirulina": "algae extract",
-  "red algae": "algae extract",
-  "sodium alginate": "algae extract",
+  'chlorella': 'algae extract',
+  'plankton': 'algae extract',
+  'seaweed': 'algae extract',
+  'spirulina': 'algae extract',
+  'red algae': 'algae extract',
+  'sodium alginate': 'algae extract',
   
   // Alternative names for carrageenan
-  "irish moss": "carrageenan",
-  "carageenan moss": "carrageenan",
-  "chondrus crispus": "carrageenan",
+  'irish moss': 'carrageenan',
+  'carageenan moss': 'carrageenan',
+  'chondrus crispus': 'carrageenan',
   
   // Alternative names for coconut oil
-  "cocos nucifera": "coconut oil",
-  "coconut butter": "coconut oil",
-  "coconut alkanes": "coconut oil",
+  'cocos nucifera': 'coconut oil',
+  'coconut butter': 'coconut oil',
+  'coconut alkanes': 'coconut oil',
   
   // Alternative names for lanolin
-  "ethoxylated lanolin": "lanolin",
-  "peg 16 lanolin": "lanolin",
-  "lanolin oil": "lanolin",
-  "wool wax": "lanolin",
-  "wool fat": "lanolin",
+  'ethoxylated lanolin': 'lanolin',
+  'peg 16 lanolin': 'lanolin',
+  'lanolin oil': 'lanolin',
+  'wool wax': 'lanolin',
+  'wool fat': 'lanolin',
   
   // Alternative names for kelp
-  "laminara digitata": "kelp",
-  "laminaria saccharina": "kelp",
-  "laminaria saccharina extract": "kelp",
+  'laminara digitata': 'kelp',
+  'laminaria saccharina': 'kelp',
+  'laminaria saccharina extract': 'kelp',
   
   // Alternative names for soybean oil
-  "glycine soja oil": "soybean oil",
-  "glycine soya (soybean) oil": "soybean oil",
-  "soya oil": "soybean oil",
+  'glycine soja oil': 'soybean oil',
+  'glycine soya (soybean) oil': 'soybean oil',
+  'soya oil': 'soybean oil',
   
   // Alternative names for red dyes
-  "d & c red #17": "red dyes",
-  "d & c red #21": "red dyes",
-  "d & c red #3": "red dyes",
-  "d & c red #30": "red dyes",
-  "d & c red #36": "red dyes",
+  'd & c red #17': 'red dyes',
+  'd & c red #21': 'red dyes',
+  'd & c red #3': 'red dyes',
+  'd & c red #30': 'red dyes',
+  'd & c red #36': 'red dyes',
   
   // Alternative names for shea butter
-  "butyrospermum parkii": "shea butter",
+  'butyrospermum parkii': 'shea butter',
   
   // Alternative names for sodium laureth sulfate
-  "laureth-23": "sodium laureth sulfate",
-  "laureth-4": "sodium laureth sulfate",
-  "sles": "sodium laureth sulfate",
+  'laureth-23': 'sodium laureth sulfate',
+  'laureth-4': 'sodium laureth sulfate',
+  'sles': 'sodium laureth sulfate',
   
   // Alternative names for sodium lauryl sulfate
-  "sls": "sodium lauryl sulfate",
+  'sls': 'sodium lauryl sulfate',
   
   // Alternative names for stearic acid
-  "stearic acid tea": "stearic acid",
+  'stearic acid tea': 'stearic acid',
   
   // Alternative names for propylene glycol monostearate
-  "pg monostearate": "propylene glycol monostearate",
+  'pg monostearate': 'propylene glycol monostearate',
   
   // Alternative names for dioctyl succinate
-  "dos": "dioctyl succinate",
-  "bis(2-ethylhexyl) succinate": "dioctyl succinate",
+  'dos': 'dioctyl succinate',
+  'bis(2-ethylhexyl) succinate': 'dioctyl succinate',
   
   // Alternative names for glyceryl stearate SE
-  "gms se": "glyceryl stearate se",
-  "glyceryl monostearate se": "glyceryl stearate se",
+  'gms se': 'glyceryl stearate se',
+  'glyceryl monostearate se': 'glyceryl stearate se',
   
   // Alternative names for glyceryl-3 diisostearate
-  "glyceryl diisostearate": "glyceryl-3 diisostearate",
-  "gdi": "glyceryl-3 diisostearate",
+  'glyceryl diisostearate': 'glyceryl-3 diisostearate',
+  'gdi': 'glyceryl-3 diisostearate',
   
   // Alternative names for hexadecyl alcohol
-  "cetyl alcohol": "hexadecyl alcohol",
+  'cetyl alcohol': 'hexadecyl alcohol',
   
   // Alternative names for hydrogenated vegetable oil
-  "hydrogenated vegetable glycerides": "hydrogenated vegetable oil",
+  'hydrogenated vegetable glycerides': 'hydrogenated vegetable oil',
   
   // Alternative names for linseed oil
-  "flaxseed oil": "linseed oil",
-  "linum usitatissimum seed oil": "linseed oil",
+  'flaxseed oil': 'linseed oil',
+  'linum usitatissimum seed oil': 'linseed oil',
   
   
   // Alternative names for sea whip extract
-  "pseudopterogorgia elisabethae": "sea whip extract",
+  'pseudopterogorgia elisabethae': 'sea whip extract',
   
   // Alternative names for shark liver oil
-  "squalene": "shark liver oil",
+  'squalene': 'shark liver oil',
   
   // Alternative names for solulan 16
-  "peg-16 lanolin": "solulan 16",
+  'peg-16 lanolin': 'solulan 16',
   
   // Alternative names for wheat germ glyceride
-  "triticum vulgare germ oil": "wheat germ glyceride"
+  'triticum vulgare germ oil': 'wheat germ glyceride'
 };
 
 // Validation function to ensure data integrity
@@ -197,7 +197,7 @@ export const findIngredients = (ingredientHtml: string): string[] => {
   validateIngredientData();
   // Split and normalize ingredients
   const ingredientsList = ingredientHtml
-    .split(",")
+    .split(',')
     .map((item) => item.trim().toLowerCase());
 
   const matchedIngredients = new Set<string>();
@@ -229,7 +229,7 @@ export const findIngredients = (ingredientHtml: string): string[] => {
     }
   }
 
-  console.log("matched");
+  console.log('matched');
   console.log(matchedIngredients);
   return Array.from(matchedIngredients);
 };
