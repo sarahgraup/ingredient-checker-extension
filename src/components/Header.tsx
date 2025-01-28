0fc7f58b81c9ba6458e632dd34635d1871e6d41f
sarahgraup <sarahgraup@g.ucla.edu>
2025-01-28 14:54:26 -0800

    added button and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Divider } from '@mui/material';
+
+
+export default function Header() {
+  return (
+    <Box>
+
+    </Box>
+  );
+
+}
---
+++ src/components/Results.tsx
@@ -0,0 +1,15 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+
+
+interface IIngredients {
+    ingredients:string[]
+}
+
+export default function Results(ingredients:IIngredients) {
+
+  return (
+    <Grid></Grid>
+  );
+    
+}
